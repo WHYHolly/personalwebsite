90bbcf1c889ace56410036fe9a9518e78a603d18
WHYHolly <hangyuwangwhy@gmail.com>
2020-05-26 15:37:19 -0400

    update build

---
+++ precache-manifest.74395d84b6c6394cd1ec346511cff6e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be01db0078a6986c42e460d7ee992ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46f49c5c753c6c857db6",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "da71c5904d51d11bc023",
+    "url": "/static/js/2.6986c962.chunk.js"
+  },
+  {
+    "revision": "443d2105bbae44cc4ccf3384197799b0",
+    "url": "/static/js/2.6986c962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f49c5c753c6c857db6",
+    "url": "/static/js/main.a3e9b88e.chunk.js"
+  },
+  {
+    "revision": "d0fe746fe6ad2bdca125",
+    "url": "/static/js/runtime-main.5a4af871.js"
+  }
+]);
